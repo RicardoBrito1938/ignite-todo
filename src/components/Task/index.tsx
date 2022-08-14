a9b08eafc12aa68d5a1151704a28ad95f37d6adb
Ricardo Brito <ricardo.jucrist@hotmail.com>
2022-08-14 12:35:01 +0100

    chore: reduce function

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,40 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { HStack, Text, useTheme } from "native-base";
+
+type TaskState = {
+  id: string | number[];
+  task: string;
+  done: boolean;
+};
+
+type Prop = {
+  data: TaskState;
+  onPress: () => void;
+  onDelete: () => void;
+};
+
+const Task = ({ data, onPress, onDelete }: Prop) => {
+  const { colors } = useTheme();
+  return (
+    <HStack alignItems="center" bg="gray.500" p={3} mb={2}>
+      <FontAwesome
+        name={data.done ? "check-circle" : "circle-thin"}
+        size={24}
+        color={colors.blueDark}
+        onPress={onPress}
+      />
+      <Text fontWeight={400} color="gray.100" flex={1} mx={2}>
+        {data.task}
+      </Text>
+
+      <FontAwesome
+        name="trash-o"
+        size={18}
+        color={colors.gray[300]}
+        onPress={onDelete}
+      />
+    </HStack>
+  );
+};
+
+export default Task;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,70 @@
-import { HStack, IconButton, useTheme, VStack } from "native-base";
+import {
+  FlatList,
+  HStack,
+  IconButton,
+  useTheme,
+  VStack,
+  Text
+} from "native-base";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
 import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
+import uuid from "react-native-uuid";
+import Task from "../../components/Task";
+
+type TaskState = {
+  id: string | number[];
+  task: string;
+  done: boolean;
+};
 
 const Home = () => {
   const { colors } = useTheme();
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<TaskState[]>([]);
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      const newList = [...tasks];
+      newList.push({
+        id: uuid.v4(),
+        task: newTask.trim(),
+        done: false
+      });
+      setTasks(newList);
+      setNewTask("");
+    }
+  };
+
+  const deleteItem = (index: number) => {
+    let newList = [...tasks];
+    newList = newList.filter((_, i) => i !== index);
+    setTasks(newList);
+  };
+
+  const toggleDone = (index: number) => {
+    let newList = [...tasks];
+    newList[index].done = !newList[index].done;
+    setTasks(newList);
+  };
+
+  const tasksClosed = tasks.reduce((acc, item) => {
+    if (item.done) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
 
   return (
-    <VStack flex={1} bg="background">
+    <VStack flex={1} bg="gray.600">
       <Header />
       <VStack px={6}>
         <HStack mt={-27}>
           <Input
+            value={newTask}
             placeholder="Add a new task"
-            onChangeText={() => {}}
+            onChangeText={setNewTask}
             flex={1}
           />
           <IconButton
@@ -25,11 +76,34 @@ const Home = () => {
                 name="pluscircleo"
                 size={16}
                 color={colors.gray[100]}
-                onPress={() => {}}
+                onPress={() => handleAddTask()}
               />
             }
           />
         </HStack>
+
+        <HStack bg="tomato" justifyContent="space-between">
+          <HStack>
+            <Text>Created</Text>
+            <Text>{tasks.length}</Text>
+          </HStack>
+          <HStack>
+            <Text>Closed</Text>
+            <Text>{tasksClosed}</Text>
+          </HStack>
+        </HStack>
+
+        <FlatList
+          data={tasks}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item, index }) => (
+            <Task
+              onPress={() => toggleDone(index)}
+              data={item}
+              onDelete={() => deleteItem(index)}
+            />
+          )}
+        />
       </VStack>
     </VStack>
   );
